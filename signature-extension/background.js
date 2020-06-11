2f37d412cd19c495136c987d4705831d442412da
Kamil Mankowski (kam193) <kam193@wp.pl>
2020-06-11 18:29:12 +0200

    Draft of authorization and setting signature from Gmail

--- signature-extension/background.js
+++ signature-extension/background.js
@@ -1,6 +1,20 @@
 browser.browserAction.onClicked.addListener(async () => {
-  accounts = await browser.accounts.list()
+  accounts = await browser.accounts.list();
   identity_id = accounts[0].identities[0].id;
-  await browser.signatureApi.setSignatureHTML(identity_id, "<b>Your new signature</b>");
-  console.log(await browser.signatureApi.getSignature(identity_id))
+
+  let signature = await getUserSignature();
+
+  await browser.signatureApi.setSignatureHTML(identity_id, signature);
+  console.log(await browser.signatureApi.getSignature(identity_id));
 });
+
+async function getUserSignature() {
+  let dataUrl = "https://www.googleapis.com/gmail/v1/users/me/settings/sendAs";
+  let token = await getAccessToken();
+  let settings = await makeRequest(token, dataUrl);
+  let signature = null;
+  settings.sendAs.forEach((alias) => {
+    if (alias.isDefault) signature = alias.signature;
+  });
+  return signature;
+}
