c7b230f486231c3dcde257627a039d9e1def863b
Kamil Mankowski (kam193) <kam193@wp.pl>
2021-05-05 19:52:22 +0200

    Scripts for packing

---
+++ Makefile
@@ -0,0 +1,5 @@
+pack:
+	./pack.sh
+
+pack_final:
+	./pack.sh ${CLIENT_ID} ${CLIENT_SECRET}
--- pack.sh
+++ pack.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+# arguments: client_id client_secret
+
 cd signature-extension
+cp -f api.js ../api.js.tmp
+sed -i "s/<CLIENT_ID>/$1/" api.js
+sed -i "s/<CLIENT_SECRET>/$2/" api.js
 zip -r ../signature-extension.xpi .
+mv -f ../api.js.tmp api.js
 cd ..
