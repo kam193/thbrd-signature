fba4c92e80a9896b0704059f8917aceb411d907f
Kamil Mankowski (kam193) <kam193@wp.pl>
2022-02-09 17:07:36 +0100

    Autoremove old package

--- pack.sh
+++ pack.sh
@@ -5,6 +5,7 @@ cd signature-extension
 cp -f api.js ../api.js.tmp
 sed -i "s/<CLIENT_ID>/$1/" api.js
 sed -i "s/<CLIENT_SECRET>/$2/" api.js
+rm -f signature-extension.xpi
 zip -r ../signature-extension.xpi .
 mv -f ../api.js.tmp api.js
 cd ..
